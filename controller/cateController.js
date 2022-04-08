98ff8b09258b40ebb634fd5e7c0555d816ce3923
jie <jiecoder@outlo0k.com>
2022-04-08 20:54:41 +0800

    完成分类列表

--- controller/cateController.js
+++ controller/cateController.js
@@ -1,10 +1,57 @@
 const path = require('path');
+const query = require('../model/query.js');
 
 const cateController = {};
 
 
+
 cateController.index = (req, res) => {
     res.render(path.resolve('views', 'catelist.html'));
+}
+
+cateController.cateData = async (req, res) => {
+    const sql = `select * from category`;
+    let queryRes = await query(sql);
+    res.json({
+        code: 0,
+        message: 'success',
+        data: queryRes
+    });
+}
+
+cateController.UpdateCateData = async (req, res) => {
+    let { id, editKey, editValue } = req.body;
+    let sql;
+    if (editKey === 'cate_name') {
+        sql = `update category set ${editKey}='${editValue}' where cate_id = ${id}`;
+    } else {
+        sql = `update category set ${editKey}= ${editValue} where cate_id = ${id}`;
+    }
+    let updateRes = await query(sql);
+    const resObj = {
+        code: 200,
+        message: '修改成功'
+    }
+    if (updateRes.affectedRows < 1) {
+        resObj.code = 201
+        resObj.message = '修改失败'
+    }
+    res.json(resObj);
+}
+
+cateController.deteleCateData = async (req, res) => {
+    let { id } = req.query;
+    const sql = `delete from category where cate_id = ${id}`;
+    let deleteRes = await query(sql);
+    const resObj = {
+        code: 200,
+        message: '删除成功'
+    }
+    if (deleteRes.affectedRows < 1) {
+        resObj.code = 201
+        resObj.message = '删除失败'
+    }
+    res.json(resObj);
 }
 
 
--- router/router.js
+++ router/router.js
@@ -5,20 +5,25 @@ const indexController = require('../controller/indexController.js');
 const cateController = require('../controller/cateController.js');
 const artController = require('../controller/artController.js');
 
-// 主页
+//页面:
+// 后台主页
 router.get('/', indexController.index);
-
 // 登录页
 router.get('/login', indexController.login);
-
-// 文章列表
+// 文章列表页
 router.get('/artlist', artController.index);
-
-// 分类列表
+// 分类列表页
 router.get('/catelist', cateController.index);
 
-// 测试列表
-router.get('/test', indexController.test);
+
+// 分类:
+// 获取分类数据
+router.get('/cateData', cateController.cateData);
+// 修改分类数据
+router.post('/UpdateCateData', cateController.UpdateCateData);
+// 删除分类数据
+router.get('/deteleCateData', cateController.deteleCateData);
+
 
 // 登录验证
 router.post('/islogin', indexController.islogin);
